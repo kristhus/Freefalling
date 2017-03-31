0e93bf2a8d7fe7d31e6535da880f592954182312
Thomas Skarshaug <thomskar@stud.ntnu.no>
2017-03-31 16:19:57 +0200

    Added all the menu classes

---
+++ app/src/main/java/com/pa_gruppe11/freefalling/gameControllers/FriendsMenu.java
@@ -0,0 +1,25 @@
+package com.pa_gruppe11.freefalling.gameControllers;
+
+import android.os.Bundle;
+
+import com.pa_gruppe11.freefalling.R;
+
+/**
+ * Created by skars on 31.03.2017.
+ */
+
+public class FriendsMenu extends GameMenu
+
+    @Override
+    public void onCreate(Bundle savedInstance){
+        super.onCreate(savedInstance);
+        setContentView(R.layout.FriendsMenu);
+    }
+
+    public void sendFriendRequest(String id) {
+
+
+    }
+
+
+}
---
+++ app/src/main/java/com/pa_gruppe11/freefalling/gameControllers/MainMenu.java
@@ -0,0 +1,38 @@
+package com.pa_gruppe11.freefalling.gameControllers;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.pa_gruppe11.freefalling.R;
+
+/**
+ * Created by skars on 31.03.2017.
+ */
+
+public class MainMenu extends GameMenu{
+
+    @Override
+    public void onCreate(Bundle savedInstance){
+        super.onCreate(savedInstance);
+        setContentView(R.layout.MainMenu);
+    }
+
+
+    public void goToAboutMenu(View view){
+        super.goTo(AboutMenu.class);
+    }
+
+    public void goToFriendsMenu(View view){
+
+    }
+
+    public void goToSettings(View view){
+
+    }
+
+    public void goToLobby(View view){
+
+    }
+
+}
+
