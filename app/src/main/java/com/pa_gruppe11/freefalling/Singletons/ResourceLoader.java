7815470ee9d1ed4e8a462e21f839fa5df65dca5c
Thomas Skarshaug <thomskar@stud.ntnu.no>
2017-03-31 13:55:20 +0200

    Implemented parts of ResourceLoader

--- app/src/main/java/com/pa_gruppe11/freefalling/Singletons/ResourceLoader.java
+++ app/src/main/java/com/pa_gruppe11/freefalling/Singletons/ResourceLoader.java
@@ -8,10 +8,30 @@ import java.util.HashMap;
  * Created by skars on 31.03.2017.
  */
 
-public class ResourceLoader {
+public final class ResourceLoader {
+
+    private static final ResourceLoader INSTANCE =  new ResourceLoader();
 
     private HashMap<Integer, ImageView> imageList;
 
+    private LoadingScreen view;
 
+    public ImageView loadImage(int id){
+
+        return imageList.get(id);
+    }
+
+    public void recycleAll(){
+        
+    }
+
+    public static ResourceLoader getInstance(){
+
+        return INSTANCE;
+    }
+
+    public HashMap getImageList(){
+        return imageList;
+    }
 
 }
