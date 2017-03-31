bf170094f075cd419b086e465b5fe55765134159
Martin Halvor Kostveit <martin_kostveit@hotmail.com>
2017-03-31 14:41:55 +0200

    canvas

--- app/src/main/java/com/pa_gruppe11/freefalling/gameControllers/GameView.java
+++ app/src/main/java/com/pa_gruppe11/freefalling/gameControllers/GameView.java
@@ -2,7 +2,10 @@ package com.pa_gruppe11.freefalling.gameControllers;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 /**
@@ -11,19 +14,35 @@ import android.view.SurfaceView;
  */
 
 public class GameView extends SurfaceView implements Runnable {
+    private SurfaceHolder holder;
+    private Drawable[] drawList;
+    private Boolean isRunning;
 
-    public GameView(Context context, AttributeSet drawableList) {
-        super(context, drawableList);
+    public GameView(Context context, Drawable[] drawableList) {
+        super(context);
+        drawList=drawableList;
+        holder = getHolder();
+        resume();
     }
 
     @Override
     public void run() {
-
+        while(isRunning) {
+            if (!holder.getSurface().isValid()) {
+                continue;
+            }
+            Canvas c = holder.lockCanvas();
+            for(int i=0; i<drawList.length; i++) {
+                drawList[i].draw(c);
+            }
+            holder.unlockCanvasAndPost(c);
+        }
     }
     public void pause() {
-
+        isRunning=false;
     }
     public void resume() {
+        isRunning=true;
 
     }
 }
