70734d5a816c3580aee19d5b5fdfa6dc5cdc9660
Martin Halvor Kostveit <martin_kostveit@hotmail.com>
2017-03-31 14:14:19 +0200

    refactoring and deleting

---
+++ app/src/main/java/com/pa_gruppe11/freefalling/gameControllers/GameActivity.java
@@ -0,0 +1,18 @@
+package com.pa_gruppe11.freefalling.gameControllers;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Martin Kostveit on 31.03.2017.
+ *
+ */
+
+public class GameActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //Drawable[] drawables = ...;
+        GameView gameView = new GameView();
+    }
+}
---
+++ app/src/main/java/com/pa_gruppe11/freefalling/gameControllers/GameView.java
@@ -0,0 +1,29 @@
+package com.pa_gruppe11.freefalling.gameControllers;
+
+import android.app.Activity;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.SurfaceView;
+
+/**
+ * Created by Martin Kostveit on 31.03.2017.
+ *
+ */
+
+public class GameView extends SurfaceView implements Runnable {
+
+    public GameView(Context context, AttributeSet drawableList) {
+        super(context, drawableList);
+    }
+
+    @Override
+    public void run() {
+
+    }
+    public void pause() {
+
+    }
+    public void resume() {
+
+    }
+}
