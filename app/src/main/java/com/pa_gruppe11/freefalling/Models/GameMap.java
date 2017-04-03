5ac63bc4f094c5649e6a75ed5062b5ed67a8e97c
Kjetil Vaagen <kjetivaa@stud.ntnu.no>
2017-04-03 15:38:25 +0200

    models med grensesnitt og arving

--- app/src/main/java/com/pa_gruppe11/freefalling/Models/GameMap.java
+++ app/src/main/java/com/pa_gruppe11/freefalling/Models/GameMap.java
@@ -1,17 +1,24 @@
 package com.pa_gruppe11.freefalling.Models;
 
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.widget.ImageView;
+
+import com.pa_gruppe11.freefalling.Drawable;
+import com.pa_gruppe11.freefalling.Singletons.ResourceLoader;
 
 /**
  * Created by kjetilvaagen on 31/03/17.
  */
 
-public class GameMap {
+public class GameMap implements Drawable {
 
     private int id;
     private ImageView image;
     private PowerUp[] powerups;
     private Obstacle[] obstacles;
+    private int x;
+    private int y;
 
 
     public GameMap(int id, ImageView image, PowerUp[] powerups, Obstacle[] obstacles){
@@ -27,5 +34,15 @@ public class GameMap {
     }
 
 
+    @Override
+    public void Drawable(int x, int y, int id) {
+        this.x = x;
+        this.y = y;
+        this.id = id;
+    }
 
+    @Override
+    public void draw(Canvas canvas) {
+        canvas.drawBitmap(ResourceLoader.getInstance().getImageList().get(id), x, y, new Paint());
+    }
 }
