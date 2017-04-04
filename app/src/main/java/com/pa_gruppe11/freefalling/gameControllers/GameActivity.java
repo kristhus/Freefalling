264569e97e64b33d1128dd8958b81d0074ea7868
Thomas Skarshaug <thomskar@stud.ntnu.no>
2017-04-04 14:57:02 +0200

    Resolved merge conflict

--- app/src/main/java/com/pa_gruppe11/freefalling/gameControllers/GameActivity.java
+++ app/src/main/java/com/pa_gruppe11/freefalling/gameControllers/GameActivity.java
@@ -1,13 +1,21 @@
 package com.pa_gruppe11.freefalling.gameControllers;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.pa_gruppe11.freefalling.Models.GameMap;
+import com.pa_gruppe11.freefalling.Models.Character;
+import com.pa_gruppe11.freefalling.R;
 import android.view.ViewGroup;
 
 import com.pa_gruppe11.freefalling.Models.GameMap;
 import com.pa_gruppe11.freefalling.Singletons.DataHandler;
 import com.pa_gruppe11.freefalling.Singletons.GameThread;
 import com.pa_gruppe11.freefalling.implementations.models.SkyStage;
+import com.pa_gruppe11.freefalling.tmp.TmpPlayer;
 import com.pa_gruppe11.freefalling.tmp.TmpView;
+import com.pa_gruppe11.freefalling.Models.Player;
 
 /**
  * Created by Kristian on 31/03/2017.
@@ -16,6 +24,7 @@ public class GameActivity extends GameMenu {
 
     private Player[] players;
     private GameMap gameMap; //
+    private Player thisPlayer; // REMOVE AFTER TESTING
 
     private PlayerController controller;
 
@@ -30,6 +39,8 @@ public class GameActivity extends GameMenu {
 
         //TODO: TESTING ONLY, REMOVE
         gameMap = new SkyStage();
+        //player = new Player(R.drawable.stickman);
+        thisPlayer = new Player(R.drawable.stickman);
 
         TmpView tmpView = new TmpView(this);
         setContentView(tmpView);
@@ -74,6 +85,10 @@ public class GameActivity extends GameMenu {
     public GameMap getGameMap() {
         return gameMap;
     }
+
+
+    // TODO: Remove after testing.
+    public Player getPlayer(){return thisPlayer;}
 
     public PlayerController getController() {
         return controller;
