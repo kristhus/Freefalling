42679e202bd32f1755f67ef41de21485de4f4190
Skarsh <skarshaug.thomas@gmail.com>
2017-04-20 19:59:10 +0200

    Have filled the game map with obstacles and set the length of the map to 6200 units.

--- app/src/main/java/com/pa_gruppe11/freefalling/implementations/models/SkyStage.java
+++ app/src/main/java/com/pa_gruppe11/freefalling/implementations/models/SkyStage.java
@@ -19,7 +19,7 @@ public class SkyStage extends GameMap {
     public SkyStage() {
         super(R.drawable.bg_sky);
 
-        endY = 8000.0f;
+        endY = 6200.0f;
         respawnPoints = new ArrayList<>();
 
         //TODO: temporary respawnpoints
@@ -31,12 +31,12 @@ public class SkyStage extends GameMap {
 
 
         // BLOCKS
-
+/*
         Block block1 = new Block(R.drawable.block, data.getRelativeWidth(40), data.getRelativeHeight(3));
         block1.setAngularVelocity((float) -Math.PI/2);
+        block1.setRotate(true);
         block1.setX(data.getRelativeX(0));
         block1.setY(600);
-        block1.setRotate(true);
 
         Block block2 = new Block(R.drawable.block, data.getRelativeWidth(40), data.getRelativeHeight(3));
         block2.setAngularVelocity((float) Math.PI/2);
@@ -53,7 +53,7 @@ public class SkyStage extends GameMap {
         Block block4 = new Block(R.drawable.block, data.getRelativeWidth(40), data.getRelativeHeight(3));
         block4.setAngularVelocity((float) Math.PI/6);
         block4.setRotate(true);
-        block4.setX(data.getRelativeX(60));
+        block4.setX(data.getRelativeX(10));
         block4.setY(2000);
 
         Block block5 = new Block(R.drawable.block, data.getRelativeWidth(40), data.getRelativeHeight(3));
@@ -64,20 +64,20 @@ public class SkyStage extends GameMap {
 
         Block block6 = new Block(R.drawable.block, data.getRelativeWidth(40), data.getRelativeHeight(3));
         block6.setAngularVelocity((float) -Math.PI/2);
-        block6.setX(data.getRelativeX(20));
+        block6.setX(data.getRelativeX(100));
         block6.setY(3450);
         block6.setRotate(true);
 
         Block block7 = new Block(R.drawable.block, data.getRelativeWidth(40), data.getRelativeHeight(3));
         block7.setAngularVelocity((float) Math.PI/2);
         block7.setRotate(true);
-        block7.setX(data.getRelativeX(80));
+        block7.setX(data.getRelativeX(200));
         block7.setY(4000);
 
         Block block8 = new Block(R.drawable.block, data.getRelativeWidth(40), data.getRelativeHeight(3));
         block8.setAngularVelocity((float) Math.PI/2);
         block8.setRotate(true);
-        block8.setX(data.getRelativeX(300));
+        block8.setX(data.getRelativeX(700));
         block8.setY(4200);
 
         Block block9 = new Block(R.drawable.block, data.getRelativeWidth(40), data.getRelativeHeight(3));
@@ -91,6 +91,63 @@ public class SkyStage extends GameMap {
         block10.setRotate(true);
         block10.setX(data.getRelativeX(200));
         block10.setY(5400);
+
+*/
+        Block block1 = new Block(R.drawable.block, data.getRelativeWidth(40), data.getRelativeHeight(3));
+        block1.setAngularVelocity((float) -Math.PI/2);
+        block1.setX(data.getRelativeX(0));
+        block1.setY(400);
+        block1.setRotate(true);
+
+        Block block2 = new Block(R.drawable.block, data.getRelativeWidth(40), data.getRelativeHeight(3));
+        block2.setAngularVelocity((float) Math.PI/2);
+        block2.setRotate(true);
+        block2.setX(data.getRelativeX(59));
+        block2.setY(400);
+
+        Block block3 = new Block(R.drawable.block, data.getRelativeWidth(40), data.getRelativeHeight(3));
+        block3.setAngularVelocity((float) Math.PI/2);
+        block3.setRotate(true);
+        block3.setX(data.getRelativeX(40));
+        block3.setY(800);
+
+        Block block4 = new Block(R.drawable.block, data.getRelativeWidth(40), data.getRelativeHeight(3));
+        block4.setAngularVelocity((float) Math.PI/2);
+        block4.setRotate(true);
+        block4.setX(data.getRelativeX(20));
+        block4.setY(1200);
+
+        Block block5 = new Block(R.drawable.block, data.getRelativeWidth(40), data.getRelativeHeight(3));
+        block5.setAngularVelocity((float) -Math.PI/4);
+        block5.setRotate(true);
+        block5.setX(data.getRelativeX(0));
+        block5.setY(1600);
+
+        Block block6 = new Block(R.drawable.block, data.getRelativeWidth(40), data.getRelativeHeight(3));
+        block6.setAngularVelocity((float) -Math.PI/2);
+        block6.setRotate(true);
+        block6.setX(data.getRelativeX(40));
+        block6.setY(2100);
+
+        Block block7 = new Block(R.drawable.block, data.getRelativeWidth(40), data.getRelativeHeight(3));
+        block7.setAngularVelocity((float) -Math.PI/4);
+        block7.setRotate(true);
+        block7.setX(data.getRelativeX(0));
+        block7.setY(2100);
+
+
+        Block block8 = new Block(R.drawable.block, data.getRelativeWidth(40), data.getRelativeHeight(3));
+        block8.setAngularVelocity((float) -Math.PI/4);
+        block8.setRotate(true);
+        block8.setX(data.getRelativeX(0));
+        block8.setY(3400);
+
+        Block block9 = new Block(R.drawable.block, data.getRelativeWidth(40), data.getRelativeHeight(3));
+        block9.setAngularVelocity((float) Math.PI/2);
+        block9.setRotate(true);
+        block9.setX(data.getRelativeX(50));
+        block9.setY(3400);
+
 
         // SPINNINGKNIVES
 
@@ -115,6 +172,12 @@ public class SkyStage extends GameMap {
         knife3.setRotate(true);
         knife3.setX(data.getRelativeX(20));
         knife3.setY(3500);
+
+        SpinningKnife knife4 = new SpinningKnife(R.drawable.knife, data.getRelativeWidth(40), data.getRelativeHeight(3.0f));
+        knife4.setAngularVelocity((float) Math.PI/2);
+        knife4.setRotate(true);
+        knife4.setX(data.getRelativeX(40));
+        knife4.setY(5800);
 
 
         // SAWBLADES
@@ -143,14 +206,29 @@ public class SkyStage extends GameMap {
         sawblade5.setPeriodic(true, 140.0f, 0.0f);
 
         Sawblade sawblade6 = new Sawblade(data.getRelativeWidth(20), data.getRelativeWidth(20));
-        sawblade6.setX(data.getRelativeX(40));
-        sawblade6.setY(5600);
-        sawblade6.setPeriodic(true, 140.0f, 0.0f);
+        sawblade5.setX(data.getRelativeX(40));
+        sawblade5.setY(3400);
+        sawblade5.setPeriodic(true, 140.0f, 0.0f);
 
-        Sawblade sawblade7 = new Sawblade(data.getRelativeWidth(100), data.getRelativeWidth(20));
-        sawblade7.setX(data.getRelativeX(300));
-        sawblade7.setY(5600);
+        Sawblade sawblade7 = new Sawblade(data.getRelativeWidth(20), data.getRelativeWidth(20));
+        sawblade7.setX(data.getRelativeX(0));
+        sawblade7.setY(4200);
         sawblade7.setPeriodic(true, 140.0f, 0.0f);
+
+        Sawblade sawblade8 = new Sawblade(data.getRelativeWidth(20), data.getRelativeWidth(20));
+        sawblade5.setX(data.getRelativeX(60));
+        sawblade5.setY(4200);
+        sawblade5.setPeriodic(true, 140.0f, 0.0f);
+
+        Sawblade sawblade9 = new Sawblade(data.getRelativeWidth(40), data.getRelativeWidth(40));
+        sawblade9.setX(data.getRelativeX(40));
+        sawblade9.setY(5600);
+        sawblade9.setPeriodic(true, 140.0f, 0.0f);
+
+        Sawblade sawblade10 = new Sawblade(data.getRelativeWidth(40), data.getRelativeWidth(40));
+        sawblade10.setX(data.getRelativeX(300));
+        sawblade10.setY(5600);
+        sawblade10.setPeriodic(true, 140.0f, 0.0f);
 
 
 
@@ -169,6 +247,12 @@ public class SkyStage extends GameMap {
         pushBlock.setPeriodic(true, 1000.0f, 0.0f);
         pushBlock.setPeriodicMaxTime(8000);
 
+        Block pushBlock3 = new Block(R.drawable.block, data.getRelativeWidth(3), data.getRelativeHeight(20));
+        pushBlock.setX(data.getRelativeX(10.0f));
+        pushBlock.setY(5800.0f);
+        pushBlock.setMaxDx(600.0f);
+        pushBlock.setPeriodic(true, 1000.0f, 0.0f);
+        pushBlock.setPeriodicMaxTime(8000);
 
         addObstacle(block1);
         addObstacle(block2);
@@ -179,8 +263,14 @@ public class SkyStage extends GameMap {
         addObstacle(block7);
         addObstacle(block8);
         addObstacle(block9);
+      /*  addObstacle(block4);
+        addObstacle(block5);
+        addObstacle(block6);
+        addObstacle(block7);
+        addObstacle(block8);
+        addObstacle(block9);
         addObstacle(block10);
-
+*/
         addObstacle(knife1);
         addObstacle(knife2);
         addObstacle(knife3);
@@ -191,8 +281,16 @@ public class SkyStage extends GameMap {
         addObstacle(sawblade4);
         addObstacle(sawblade5);
         addObstacle(sawblade6);
+        addObstacle(sawblade7);
+        addObstacle(sawblade8);
+        addObstacle(sawblade9);
+        addObstacle(sawblade10);
+
+        // Push blocks
+
         addObstacle(pushBlock);
         addObstacle(pushBlock2);
+        addObstacle(pushBlock3);
 
 
     }
